71ae2a1ad48e56f8ce8cdf4a2ce2d0cce923d6d1
Manuel Lara <manuellaraa@gmail.com>
2022-07-20 16:23:48 -0700

    first commit

---
+++ utils/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseAnonKey  = process.env.ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
